8cf29e4e7a2f6ed6249daced267aa0cc8e64ab29
William <nettee.liu@gmail.com>
2018-09-29 17:09:27 +0800

    update

--- handle.py
+++ handle.py
@@ -11,16 +11,15 @@ class Handle(object):
         try:
             webData = web.data()
             print "Handle Post webdata is ", webData
-   #后台打日志
             recMsg = receive.parse_xml(webData)
             if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
                 toUser = recMsg.FromUserName
                 fromUser = recMsg.ToUserName
-                content = "test"
+                content = recMsg.Content + "test"
                 replyMsg = reply.TextMsg(toUser, fromUser, content)
                 return replyMsg.send()
             else:
                 print "暂且不处理"
                 return "success"
-        except Exception, Argment:
-            return Argment
+        except Exception as e:
+            return e
