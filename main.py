0fc06b1f901c9dd1f3c67d3f0601444310372118
William <nettee.liu@gmail.com>
2018-10-01 11:02:45 +0800

    switch to python3

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# filename: main.py
-
 from flask import Flask, request
 
 import receive
@@ -8,21 +5,22 @@ import reply
 
 app = Flask(__name__)
 
+
 @app.route('/wx', methods=['POST'])
 def weixin():
     assert request.method == 'POST'
     try:
         webData = request.data
-        print "Handle Post webdata is ", webData
+        print("Handle Post webdata is", webData)
         recMsg = receive.parse_xml(webData)
         if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
             toUser = recMsg.FromUserName
             fromUser = recMsg.ToUserName
-            content = "你好，谢谢你的消息！你的消息一共有" + str(len(recMsg.Content)) + "个字"
+            content = "你好，谢谢你的消息！你的消息一共有{}个字".format(len(recMsg.Content))
             replyMsg = reply.TextMsg(toUser, fromUser, content)
             return replyMsg.send()
         else:
-            print "暂且不处理"
+            print("暂且不处理")
             return "success"
     except Exception as e:
         return e
