3e36138d55349c49487806a08a5f9daf608ea00f
William <nettee.liu@gmail.com>
2018-10-01 10:52:35 +0800

    switch to Flask

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 # filename: main.py
-import web
-from handle import Handle
 
-urls = (
-    '/wx', 'Handle',
-)
+from flask import app, request
 
-if __name__ == '__main__':
-    app = web.application(urls, globals())
-    app.run()
+import receive
+import reply
+
+@app.route('/wx', methods=['POST'])
+def weixin():
+    assert request.method == 'POST'
+    try:
+        webData = request.data
+        print "Handle Post webdata is ", webData
+        recMsg = receive.parse_xml(webData)
+        if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+            toUser = recMsg.FromUserName
+            fromUser = recMsg.ToUserName
+            content = "你好，谢谢你的消息！你的消息一共有" + str(len(recMsg.Content)) + "个字"
+            replyMsg = reply.TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+        else:
+            print "暂且不处理"
+            return "success"
+    except Exception as e:
+        return e
